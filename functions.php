1c3ee0ea26679e697dea78829c56a422865d1e48
Apple <apple@redlimesolutions.com>
2021-12-23 23:42:27 +0600

    enqueue done, change inside header tag

--- functions.php
+++ functions.php
@@ -141,9 +141,22 @@ add_action( 'widgets_init', 'noonpost_widgets_init' );
  */
 function noonpost_scripts() {
 	wp_enqueue_style( 'noonpost-style', get_stylesheet_uri(), array(), NP_VERSION );
+	wp_enqueue_style( 'noonpost-googlefonts','https://fonts.googleapis.com/css?family=Muli:300,400,500,600,700,800,900&amp;display=swap', false);
+	wp_enqueue_style( 'noonpost-all', get_template_directory_uri() . '/assets/css/all.css', array(), NP_VERSION );
+	wp_enqueue_style( 'elegant-font-icons', get_template_directory_uri() . '/assets/css/elegant-font-icons.css', array(), NP_VERSION );
+	wp_enqueue_style( 'bootstrap', get_template_directory_uri() . '/assets/css/bootstrap.min.css', array(), NP_VERSION );
+	wp_enqueue_style( 'owl-carousel', get_template_directory_uri() . '/assets/css/owl.carousel.css', array(), NP_VERSION );
+	wp_enqueue_style( 'main-style', get_template_directory_uri() . '/assets/css/style.css', array(), NP_VERSION );
+	wp_enqueue_style( 'custom', get_template_directory_uri() . '/assets/css/custom.css', array(), NP_VERSION );
 	wp_style_add_data( 'noonpost-style', 'rtl', 'replace' );
 
 	wp_enqueue_script( 'noonpost-navigation', get_template_directory_uri() . '/js/navigation.js', array(), NP_VERSION, true );
+	wp_enqueue_script( 'popper', get_template_directory_uri() . '/assets/js/popper.min.js', array('jquery'), NP_VERSION, true );
+	wp_enqueue_script( 'bootstrap', get_template_directory_uri() . '/assets/js/bootstrap.min.js', array('jquery'), NP_VERSION, true );
+	wp_enqueue_script( 'ajax-contact', get_template_directory_uri() . '/assets/js/ajax-contact.js', array('jquery'), NP_VERSION, true );
+	wp_enqueue_script( 'owl-carousel', get_template_directory_uri() . '/assets/js/owl.carousel.min.js', array('jquery'), NP_VERSION, true );
+	wp_enqueue_script( 'switch', get_template_directory_uri() . '/assets/js/switch.js', array('jquery'), NP_VERSION, true );
+	wp_enqueue_script( 'main', get_template_directory_uri() . '/assets/js/main.js', array('jquery'), NP_VERSION, true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
